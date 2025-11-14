ba45dbf0ccdeb8ab0b4ceef8d0fe5fdd6962e577
abiramiganesan2005 <abiramiganesan2005@gmail.com>
2025-11-14 12:05:54 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/digital_skill_page/',   // <-- EXACT repo name with leading & trailing slash
   plugins: [react()],
 })
